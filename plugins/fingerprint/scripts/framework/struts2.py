e5bb5a6b9fed68bb9cc9a433820ca039cda6be03
recar <1766241489@qq.com>
2022-04-01 15:10:01 +0800

    fix

--- plugins/fingerprint/scripts/framework/struts2.py
+++ plugins/fingerprint/scripts/framework/struts2.py
@@ -3,7 +3,7 @@
 '''
 Date: 2022-03-21 20:39:10
 LastEditors: recar
-LastEditTime: 2022-04-01 11:47:28
+LastEditTime: 2022-04-01 15:09:48
 '''
 
 from plugins.fingerprint.scripts.base import FingerprintCheck
@@ -12,7 +12,7 @@ class Check(FingerprintCheck):
     def __init__(self):
         super(Check, self).__init__()
     
-    def verify(self, url_info, req, rsp)):
+    def verify(self, url_info, req, rsp):
         self.name = "struts2"
         set_cookie = rsp.get("headers").headers.get("Set-Cookie")
         if set_cookie.startswith("JSESSIONID="):
--- plugins/fingerprint/scripts/framework/thinkphp.py
+++ plugins/fingerprint/scripts/framework/thinkphp.py
@@ -3,7 +3,7 @@
 '''
 Date: 2022-03-17 17:09:23
 LastEditors: recar
-LastEditTime: 2022-04-01 11:38:50
+LastEditTime: 2022-04-01 15:09:34
 '''
 
 from plugins.fingerprint.scripts.base import FingerprintCheck
@@ -12,7 +12,7 @@ class Check(FingerprintCheck):
     def __init__(self):
         super(Check, self).__init__()
     
-    def verify(self, url_info):
+    def verify(self, url_info, req, rsp):
         host = url_info.get('base_url')
         self.name = "thinkphp"
         url = f"{host}/?c=4e5e5d7364f443e28fbf0d3ae744a59a"
